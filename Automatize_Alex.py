8d0fcd29235c5a3b647b52d65575b92ade0c9def
AlexAlbino <84361166+AlexAlbino@users.noreply.github.com>
2021-10-17 14:31:56 -0300

    Update Automatize_Alex.py

--- Automatize_Alex.py
+++ Automatize_Alex.py
@@ -1,12 +1,12 @@
 ## Consolidando arquivos
 import os
 import pandas as pd
-pasta = 'C:/Users/AlexA/OneDrive/Área de Trabalho/Filiais 2'
+pasta = 'endereço do diretório'
 df = pd.DataFrame()
 for diretorio, subpastas, arquivos in os.walk(pasta):
     for arquivo in arquivos:
         endereco = os.path.join(diretorio, arquivo)
         dados = pd.read_excel(endereco)
         df = df.append(dados)
-df.to_excel('C:/Users/AlexA/OneDrive/Área de Trabalho/Filiais 2/Teste.xlsx', index= False)
+df.to_excel('caminho onde deseja salvar o novo arquivo' & /nome_arquivo.xlsx', index= False)
 print("Concluido")
